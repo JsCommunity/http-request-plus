9a55f39b8693262e2a461aecb2566619b3e5d827
Julien Fontanet <julien.fontanet@isonoe.net>
2016-10-26 15:55:13 +0200

    chore(package): use jest for tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,5 @@
-/* eslint-env mocha */
+/* eslint-env jest */
 
-import expect from 'must'
 import { createServer as createHttpServer } from 'http'
 
 import httpRequestPlus from './'
@@ -10,10 +9,10 @@ import httpRequestPlus from './'
 describe('httpRequestPlus', () => {
   let server
   let port
-  before(done => {
-    server = createHttpServer((req, res) => server.emit(req.url, req, res))
-
-    server.listen(0, 'localhost', error => {
+  beforeAll(done => {
+    server = createHttpServer((req, res) =>
+      server.emit(req.url, req, res)
+    ).listen(0, 'localhost', error => {
       if (error) {
         return done(error)
       }
@@ -22,15 +21,15 @@ describe('httpRequestPlus', () => {
       done()
     })
   })
-  after(done => {
+  afterAll(done => {
     server.close(done)
   })
 
   it('works', () => {
     server.once('/foo', (req, res) => res.end('bar'))
 
-    return expect(
-      httpRequestPlus({ port, path: '/foo' }).readAll('utf-8')
-    ).to.resolve.to.equal('bar')
+    return httpRequestPlus({ port, path: '/foo' }).readAll('utf-8').then(body => {
+      expect(body).toBe('bar')
+    })
   })
 })

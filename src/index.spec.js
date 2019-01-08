05905820a83d1c1a48d73d7df3923b4e5f77785a
Julien Fontanet <julien.fontanet@isonoe.net>
2019-01-08 18:10:56 +0100

    chore: add tests on cancelation

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 
+import { Cancel, CancelToken } from "promise-toolbox";
 import { createServer as createHttpServer } from "http";
 
 import httpRequestPlus from "./";
@@ -17,6 +18,14 @@ const redirect = (res, url) => {
   });
   res.end();
 };
+
+const rejectionOf = p =>
+  p.then(
+    _ => {
+      throw _;
+    },
+    _ => _
+  );
 
 // ===================================================================
 
@@ -83,6 +92,30 @@ describe("httpRequestPlus", () => {
       return httpRequestPlus({ port, path: "/foo" }).then(res => {
         expect(res.url).toBe(`http://localhost:${port}/bar`);
       });
+    });
+  });
+
+  describe("CancelToken", () => {
+    it("can cancel the request", async () => {
+      const { cancel, token } = CancelToken.source();
+
+      cancel();
+      expect(
+        await rejectionOf(httpRequestPlus(token, { port }))
+      ).toBeInstanceOf(Cancel);
+    });
+
+    it("can cancel the response which emit an error event", async () => {
+      // something needs to be written to close finish the request
+      server.once("/", (req, res) => res.write("foo"));
+
+      const { cancel, token } = CancelToken.source();
+
+      const r = await httpRequestPlus(token, { port });
+      cancel();
+      expect(
+        await new Promise(resolve => r.on("error", resolve))
+      ).toBeInstanceOf(Cancel);
     });
   });
 });
